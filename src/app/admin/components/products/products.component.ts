c56363d0a815a4d4a9e17549fa6350ecf9aa5eb3
Batu YURT <asdasd018@hotmail.com>
2022-09-10 20:50:28 +0300

    Custom httpclient service

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/contracts/product';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpClientService: HttpClientService) { }
 
   ngOnInit(): void {
+
+    this.httpClientService.get<Product[]>({
+      controller: "products"
+    }).subscribe(data => console.log(data));
+
+
+    // this.httpClientService.post({
+    //   controller: "products"
+    // }, {
+    //   name: "kalem",
+    //   stock: 100,
+    //   price: 15
+
+    // }).subscribe();
+
+
+    // this.httpClientService.put({
+    //   controller: "products"
+    // }, {
+    //   id: "07cfbe25-6477-4a6b-8388-168d536074ee",
+    //   name: "renkli kağıt",
+    //   stock: 300,
+    //   price: 9
+
+    // }).subscribe()
+
+
+    // this.httpClientService.delete({
+    //   controller: "products"
+    // },
+    //   "3d783f53-7cb5-4c19-8c87-1ca5086e039c"
+    // ).subscribe()
+
+// this.httpClientService.get({
+//   fullEndPoint: "https://jsonplaceholder.typicode.com/posts"
+// }).subscribe(data => console.log(data))
+
   }
 
 }
