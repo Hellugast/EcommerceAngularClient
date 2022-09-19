d16bff502ccee9fc39583f122ea0d1584b971b0c
Batu YURT <asdasd018@hotmail.com>
2022-09-19 23:29:43 +0300

    File upload service

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -3,6 +3,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerTypes } from 'src/app/base/base.component';
 import { Create_Product } from 'src/app/contracts/create_product';
 import { AlertifyOptions, AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { FileUploadOptions } from 'src/app/services/common/file-upload/file-upload.component';
 import { ProductService } from 'src/app/services/common/models/product.service';
 
 @Component({
@@ -17,28 +18,35 @@ export class CreateComponent extends BaseComponent implements OnInit {
   ngOnInit(): void {
   }
 
-@Output() createdProduct: EventEmitter<Create_Product> = new EventEmitter();
-
-  create(name: HTMLInputElement, stock: HTMLInputElement, price: HTMLInputElement) {
-    this.showSpinner(SpinnerTypes.BallAtom)
-    const create_product: Create_Product = new Create_Product();
-    create_product.name = name.value;
-    create_product.stock = parseInt(stock.value);
-    create_product.price = parseFloat(price.value);
-
-    this.productService.create(create_product, () => {
-      this.hideSpinner(SpinnerTypes.BallAtom);
-      this.alertify.message("ürün başarıyla eklendi", {
-        messageType: MessageType.Success,
-        position: Position.TopRight
-      });
-      this.createdProduct.emit(create_product);
-    }, errorMessage => {
-      this.alertify.message(errorMessage,
-        {
-          messageType: MessageType.Error,
-          position: Position.TopRight
-        })
-    });
+  @Output() createdProduct: EventEmitter<Create_Product> = new EventEmitter();
+  @Output() fileUploadOptions : Partial<FileUploadOptions> = {
+    action: "upload",
+    controller: "products",
+    explanation: "Resimleri seçin",
+    isAdminPage: true,
+    accept: ".png, .jpg, jpeg"
   }
+
+    create(name: HTMLInputElement, stock: HTMLInputElement, price: HTMLInputElement) {
+  this.showSpinner(SpinnerTypes.BallAtom)
+  const create_product: Create_Product = new Create_Product();
+  create_product.name = name.value;
+  create_product.stock = parseInt(stock.value);
+  create_product.price = parseFloat(price.value);
+
+  this.productService.create(create_product, () => {
+    this.hideSpinner(SpinnerTypes.BallAtom);
+    this.alertify.message("ürün başarıyla eklendi", {
+      messageType: MessageType.Success,
+      position: Position.TopRight
+    });
+    this.createdProduct.emit(create_product);
+  }, errorMessage => {
+    this.alertify.message(errorMessage,
+      {
+        messageType: MessageType.Error,
+        position: Position.TopRight
+      })
+  });
+}
 }
