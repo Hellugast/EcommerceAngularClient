ca3efd1baebef037842ce707215a6be8b4d2ca52
Batu YURT <asdasd018@hotmail.com>
2022-10-06 23:51:25 +0300

    Google login

--- src/app/ui/components/login/login.component.ts
+++ src/app/ui/components/login/login.component.ts
@@ -1,9 +1,12 @@
+import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerTypes } from 'src/app/base/base.component';
+import { TokenResponse } from 'src/app/contracts/tokens/tokenResponse';
 import { User } from 'src/app/entities/user';
 import { AuthService } from 'src/app/services/common/auth.service';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 import { UserService } from 'src/app/services/common/models/user.service';
 
 @Component({
@@ -14,8 +17,17 @@ import { UserService } from 'src/app/services/common/models/user.service';
 export class LoginComponent extends BaseComponent implements OnInit {
 
   constructor(private userService: UserService, spinner: NgxSpinnerService,
-    private authService: AuthService, private activatedRoute: ActivatedRoute, private router: Router) {
+    private authService: AuthService, private activatedRoute: ActivatedRoute,
+    private router: Router, private socialAuthService: SocialAuthService, httpClientService: HttpClientService) {
     super(spinner)
+    spinner.show(SpinnerTypes.BallAtom)
+    socialAuthService.authState.subscribe(async (user: SocialUser) => {
+
+      await userService.googleLogin(user, () => {
+        this.authService.identityCheck();
+        this.hideSpinner(SpinnerTypes.BallAtom)
+      });
+    });
   }
 
   ngOnInit(): void {
