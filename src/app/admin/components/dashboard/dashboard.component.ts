41f2552ed6149d5ce86e81f51e6a35bf0b97ae18
Batu YURT <asdasd018@hotmail.com>
2022-10-16 18:47:32 +0300

    SignalR service

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerTypes } from 'src/app/base/base.component';
+import { HubUrls } from 'src/app/constants/hub-url';
+import { ReceiveFunctions } from 'src/app/constants/receive-functions';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { SignalRService } from 'src/app/services/common/signalr.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,11 +13,18 @@ import { BaseComponent, SpinnerTypes } from 'src/app/base/base.component';
 })
 export class DashboardComponent extends BaseComponent implements OnInit {
 
-  constructor(spinner: NgxSpinnerService) {
+  constructor(spinner: NgxSpinnerService, private signalRService: SignalRService, private alertifyService: AlertifyService) {
     super(spinner)
-   }
+    signalRService.start(HubUrls.ProductHub)
+  }
 
   ngOnInit(): void {
+    this.signalRService.on(ReceiveFunctions.ProductAddedMessageReceiveFunction, message => {
+      this.alertifyService.message(message, {
+        messageType: MessageType.Notify,
+        position: Position.TopCenter
+      })
+    })
     this.showSpinner(SpinnerTypes.BallScale)
   }
 
