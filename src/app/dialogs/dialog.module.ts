18755f3e8e23a1e17d0ad3afa0a0daa68da34ebb
Batu YURT <asdasd018@hotmail.com>
2022-11-10 23:44:20 +0300

    Role based authorization page

--- src/app/dialogs/dialog.module.ts
+++ src/app/dialogs/dialog.module.ts
@@ -14,6 +14,8 @@ import { OrderDetailDialogComponent } from './order-detail-dialog/order-detail-d
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { CompleteOrderDialogComponent } from './complete-order-dialog/complete-order-dialog.component';
+import { AuthorizeMenuDialogComponent } from './authorize-menu-dialog/authorize-menu-dialog.component';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,8 @@ import { CompleteOrderDialogComponent } from './complete-order-dialog/complete-o
     BasketItemRemoveDialogComponent,
     CompleteShoppingDialogComponent,
     OrderDetailDialogComponent,
-    CompleteOrderDialogComponent
+    CompleteOrderDialogComponent,
+    AuthorizeMenuDialogComponent
   ],
   imports: [
     CommonModule,
@@ -32,7 +35,8 @@ import { CompleteOrderDialogComponent } from './complete-order-dialog/complete-o
     MatCardModule,
     FormsModule,
     MatTableModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatBadgeModule
   ]
 })
 export class DialogModule { }
